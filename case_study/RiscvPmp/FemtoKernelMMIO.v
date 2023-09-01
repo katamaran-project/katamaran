71fcee47fd4c9ba52add86fd50b40e7e93f46643
Thomas Van Strydonck <Thomas.VanStrydonck@cs.kuleuven.be>
2023-09-01 11:35:25 +0200

    Create separate file for MMIO femtokernel

---
+++ case_study/RiscvPmp/FemtoKernelMMIO.v
@@ -0,0 +1,66 @@
+(******************************************************************************)
+(* Copyright (c) 2020 Steven Keuchel, Dominique Devriese, Sander Huyghebaert  *)
+(* All rights reserved.                                                       *)
+(*                                                                            *)
+(* Redistribution and use in source and binary forms, with or without         *)
+(* modification, are permitted provided that the following conditions are     *)
+(* met:                                                                       *)
+(*                                                                            *)
+(* 1. Redistributions of source code must retain the above copyright notice,  *)
+(*    this list of conditions and the following disclaimer.                   *)
+(*                                                                            *)
+(* 2. Redistributions in binary form must reproduce the above copyright       *)
+(*    notice, this list of conditions and the following disclaimer in the     *)
+(*    documentation and/or other materials provided with the distribution.    *)
+(*                                                                            *)
+(* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS        *)
+(* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  *)
+(* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR *)
+(* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR          *)
+(* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,      *)
+(* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,        *)
+(* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR         *)
+(* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF     *)
+(* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING       *)
+(* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS         *)
+(* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               *)
+(******************************************************************************)
+
+From Coq Require Import
+     ZArith.ZArith
+     Lists.List
+     Strings.String.
+From Katamaran Require Import
+     Bitvector
+     BitvectorSolve
+     Notations
+     Specification
+     SmallStep.Step
+     RiscvPmp.BlockVer.Spec
+     RiscvPmp.BlockVer.Verifier
+     RiscvPmp.IrisModel
+     RiscvPmp.IrisInstance
+     RiscvPmp.Machine
+     RiscvPmp.PmpCheck
+     RiscvPmp.Sig.
+From Katamaran Require
+     RiscvPmp.Contracts
+     RiscvPmp.LoopVerification
+     RiscvPmp.Model.
+From iris.base_logic Require lib.gen_heap lib.iprop invariants.
+From iris.bi Require interface big_op.
+From iris.algebra Require dfrac big_op.
+From iris.program_logic Require weakestpre adequacy.
+From iris.proofmode Require string_ident tactics.
+From stdpp Require namespaces.
+
+Import RiscvPmpProgram.
+
+Set Implicit Arguments.
+Import ctx.resolution.
+Import ctx.notations.
+Import env.notations.
+Import ListNotations.
+Open Scope ctx_scope.
+
+Module inv := invariants.
