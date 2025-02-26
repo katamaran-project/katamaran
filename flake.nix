063f9be59aefb03bef3b9425c3e7d8f6a78c2c91
Steven Keuchel <steven.keuchel@gmail.com>
2025-02-26 12:26:12 +0100

    flake: default to coq-8.19

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     in
     {
       devShells = rec {
-        default = coq820;
+        default = coq819;
         coq819 = pkgs.mkShell { buildInputs = coqPackages819; };
         coq820 = pkgs.mkShell { buildInputs = coqPackages820; };
 
