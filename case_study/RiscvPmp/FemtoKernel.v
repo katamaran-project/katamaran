cfb22e5e3d1f1cd82dd53454b208e5e06fad6c62
Fernand Dubler <ferdifly@pm.me>
2025-09-26 12:21:29 +0200

    wip - poc mmio may write only even values

--- case_study/RiscvPmp/FemtoKernel.v
+++ case_study/RiscvPmp/FemtoKernel.v
@@ -213,7 +213,7 @@ Module inv := invariants.
 
     Example femtokernel_handler_asm : list ASM :=
       [
-        ITYPE bv.zero zero zero RISCV_ADDI (* padding instruction to pass lemma adv_mmio_eq *)
+        RTYPE t0 t0 zero RISCV_ADD
       ; ITYPE bv.zero zero ra RISCV_ADDI
       ; AnnotLemmaInvocation (close_mmio_write (bv.of_N mmio_write_addr) WORD) [nenv exp_val ty_xlenbits bv.zero;  exp_val ty_regno t0]%env (* TODO: notation to avoid lemma call copying LOAD instruction/internalize immediate as well?*)
       ; Λ x, STORE (bv.of_N mmio_write_addr) t0 ra WORD (* works because mmio_write_addr fits into 12 bits. *)
