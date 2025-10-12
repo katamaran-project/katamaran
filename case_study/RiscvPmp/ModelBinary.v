9801c8281d3cbeab05316d4f82b976717b75c334
Fernand Dubler <ferdifly@pm.me>
2025-10-12 15:07:09 +0200

    Restore ModelBinary.v

--- case_study/RiscvPmp/ModelBinary.v
+++ case_study/RiscvPmp/ModelBinary.v
@@ -541,19 +541,11 @@ Module RiscvPmpModel2.
       iApply ("Hwithout" with "Hacc").
     Qed.
 
-    Lemma close_mmio_write_sound (imm : bv 12) (width : WordWidth):
-      ValidLemma (RiscvPmpSpecification.lemma_close_mmio_write imm width).
-    Proof.
-      intros ι; destruct_syminstance ι; cbn.
-      now iIntros.
-    Qed.
-
-
     Lemma lemSem : LemmaSem.
     Proof.
       intros Δ [];
         eauto using open_gprs_sound, close_gprs_sound, open_ptsto_instr_sound, close_ptsto_instr_sound, open_pmp_entries_sound,
-        close_pmp_entries_sound, extract_pmp_ptsto_sound, return_pmp_ptsto_sound, close_mmio_write_sound.
+        close_pmp_entries_sound, extract_pmp_ptsto_sound, return_pmp_ptsto_sound.
     Qed.
   End LemProofs.
 
