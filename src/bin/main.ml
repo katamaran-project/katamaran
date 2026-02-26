7987f4521f4fec7eab548f4cf27e0af113b08c0b
Denis Carnier <decrn@users.noreply.github.com>
2026-02-26 17:16:08 +0100

    Add some glue code

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,4 +1,11 @@
+
 let () = 
-  let result = BlockVerifier.exec_VC _ in
-  print_endline "Finished"
+  let triple =
+    { precondition = _;
+      instr = _;
+      postcondition = _ }
+  in
+  let string_of_symprop s = _ in
+  let result = BlockVerifier.exec_VC BlockVerifier.Coq_nil triple in
+  print_endline (string_of_symprop result)
 
